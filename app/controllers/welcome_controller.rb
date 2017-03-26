414bad6b5eb14e3ad869b72c1f8d6de340c99472
dumpling001 <dumpling001@gmail.com>
2017-03-26 14:59:07 +0800

    add some flash info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "午安！你好！你会成功的！：）bigbigsmile！"
+    flash[:alert] = "做完作业就要出去玩哦！每天争取一万步！"
+    flash[:warning] = "记得多喝水！"
+  end
 end
